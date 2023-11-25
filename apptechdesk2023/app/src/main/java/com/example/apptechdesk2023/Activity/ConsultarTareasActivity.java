4b598512f6201570bb80a4f96bb542870821e6ce
mlerma <mariolerma0396@gmail.com>
2023-11-25 18:32:19 -0500

    Modulos "Crear proyectos" y "Tus soluciones"

---
+++ apptechdesk2023/app/src/main/java/com/example/apptechdesk2023/Activity/ConsultarTareasActivity.java
@@ -0,0 +1,16 @@
+package com.example.apptechdesk2023.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.apptechdesk2023.R;
+
+public class ConsultarTareasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consultartareas);
+    }
+}
---
+++ apptechdesk2023/app/src/main/java/com/example/apptechdesk2023/entidades/Proyectos.java
@@ -0,0 +1,45 @@
+package com.example.apptechdesk2023.entidades;
+
+public class Proyectos {
+
+    private int id;
+    private String nombre;
+    private String descripcion;
+    private String tipo;
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
